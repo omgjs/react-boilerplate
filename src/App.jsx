72cb3102de0fb8b7b669b02d444ef4edbde4d633
Artem Korolev <artkor@live.ru>
2018-10-18 19:31:46 +0300

    initial commit for boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Home from "pages/Home";
+
+const App = () => <Home />;
+
+ReactDOM.render(<App />, document.getElementById("app"));
